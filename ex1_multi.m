b001665bd6648b8f2ef02056e6e2651dcc6bc197
prajithlal <prajithlal@users.noreply.github.com>
2015-02-24 14:11:24 +0530

    Update ex1_multi.m

--- ex1_multi.m
+++ ex1_multi.m
@@ -83,7 +83,7 @@ fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 0.01;
-num_iters = 400;
+num_iters = 4000;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -105,7 +105,16 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = [1, 1.6, .003] *theta; % You should change this
+
+price=[1650,3];
+%mu=[ones(1,1) mu];
+%sigma=[ones(1,1) sigma];
+
+price_norm=bsxfun(@minus,price,mu);
+price_norm=bsxfun(@rdivide,price_norm,sigma);
+price=[ones(1,1) price_norm];
+
+price = price*theta; % You should change this
 
 
 % ============================================================
@@ -150,7 +159,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = [1, 1.6, .003] *theta; % You should change this
+price = [1, 1650, 3] *theta; % You should change this
 
 
 % ============================================================
