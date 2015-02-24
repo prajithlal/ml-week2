9aafa8177c2b39c61675f44fa1e6e1d1ce7570b5
prajithlal <prajithlal@users.noreply.github.com>
2015-02-24 12:18:31 +0530

    Update ex1_multi.m

--- ex1_multi.m
+++ ex1_multi.m
@@ -95,6 +95,7 @@ plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
+
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
@@ -104,7 +105,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = [1, 1.6, .003] *theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1.6, .003] *theta; % You should change this
 
 
 % ============================================================
